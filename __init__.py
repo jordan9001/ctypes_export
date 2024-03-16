458108d7bdda393747f05d75660c62c6954d2009
Jordan Whitehead <jordan9001@gmail.com>
2024-03-16 14:22:33 -0600

    Still getting bugs outw

--- __init__.py
+++ __init__.py
@@ -28,8 +28,14 @@ structunion_definition_template = """{prefix}{typename}._fields_ = [
 ]
 """
 
+structunion_line_template = """    ('{name}', {equiv}),
+"""
+
 enum_template = """class {prefix}{typename}(enum.IntEnum):
 {items}
+"""
+
+enum_line_template = """    {name} = {val}
 """
 
 alias_template = """{prefix}{typename} = {equiv}"""
@@ -67,32 +73,151 @@ def get_type_kind(bv, tobj, tname):
         refobj = gt(bv, tname)
         if refobj is None:
             raise NameError(f"Could not find referenced type {tname}")
-        return get_type_kind(bv, refobj, tname)
+        
+        name = tname
+        if type(refobj) == NamedTypeReferenceType:
+            # do we need an alias here?
+            name = refobj.name
+        return get_type_kind(bv, refobj, name)
 
     if tobj_type in [ArrayType, BoolType, CharType, FloatType, FunctionType, IntegerType, PointerType, VoidType, WideCharType]:
         return TypeKind.ALIAS
     
     raise NotImplementedError(f"Unimplemented export of type with tobj_type {tobj_type}")
 
-def get_structure_preitems(tobj, tname, prefix):
-    # define anonymous structures and unions for this type
-    #TODO
-    raise NotImplementedError()
+def make_anon_name(structmem, parent):
+    name = f'{parent}__0x{structmem.offset:x}'
+    return name
 
-def get_structure_items(tobj, tname, prefix):
+def get_structunion_preitems(bv, tobj, tname, prefix):
+    report = ""
+    # define anonymous structures and unions for this type
+    for m in tobj.members:
+        if m.type == StructureType:
+            # this is not a NamedTypeReferenceType so it must be anonymous
+            name = make_anon_name(m, tname)
+            report += full_definition(bv, name, m.type, prefix)
+        #TODO handle unnamed structures used in pointer, arrays, functions
+    
+    return report
+
+def structunion_line(structmem, parent, prefix):
+    name = structmem.name
+    if len(name) == 0:
+        name = f"__0x{structmem.offset:x}"
+
+    equiv = None
+    mobj_type = type(structmem.type)
+
+    # if it is a structure or enum or named type, we can use our name
+    if mobj_type == NamedTypeReferenceType:
+        # use the name
+        equiv = f"{prefix}{structmem.type.name}"
+    elif mobj_type == StructureType:
+        equiv = prefix + make_anon_name(structmem, parent)
+    elif mobj_type == EnumerationType:
+        tref = structmem.type.registered_name
+        if tref is None:
+            raise NotImplementedError("Unhandled case, raw enumeration type in structure member with no registered name")
+        equiv = f"{prefix}{tref.name}"
+    else:
+        # ArrayType, BoolType, CharType, FloatType, FunctionType, IntegerType, PointerType, VoidType, WideCharType
+        equiv = get_ctypes_equiv(structmem.type, prefix)
+
+    return structunion_line_template.format(name=name, equiv=equiv)
+
+def get_union_items(tobj, tname, prefix):
+    items = ""
+
+    for m in tobj.members:
+        items += structunion_line(m, tname, prefix)
+
+    return items
+
+def get_struct_items(tobj, tname, prefix):
+    items = ""
     # define fields
-    #TODO
-    raise NotImplementedError()
+    offset = 0
+    for m in tobj.members:
+        while offset < m.offset:
+            # add padding
+            items += structunion_line_template.format(name=f"pad_0x{offset:x}", equiv="ctypes.c_uint8")
+            offset += 1
+
+        # add item
+        items += structunion_line(m, tname, prefix)
+        
+    # pad at end
+    while offset < tobj.width:
+        items += structunion_line_template.format(name=f"pad_0x{offset:x}", equiv="ctypes.c_uint8")
+        offset += 1
+
+    return items
 
 def get_enum_items(tobj):
     # define enum options
-    #TODO
-    raise NotImplementedError()
+    items = ""
+    for m in tobj.members:
+        items += enum_line_template.format(name=m.name, val=m.value)
 
-def get_alias_equiv(tobj):
+    return items
+
+def get_ctypes_equiv(tobj, prefix):
     # get ctypes equivalent string
-    #TODO
-    raise NotImplementedError()
+
+    tobj_type = type(tobj)
+    if tobj_type == NamedTypeReferenceType:
+        return f"{prefix}{tobj.name}"
+    elif tobj_type == ArrayType:
+        subtype = get_ctypes_equiv(tobj.element_type, prefix)
+        return f"({subtype}) * {tobj.count}"
+    elif tobj_type in [BoolType, CharType, IntegerType, WideCharType]:
+        signed = False
+        if tobj_type == IntegerType:
+            signed = tobj.signed
+
+        width = tobj.width
+        if width not in [1,2,4,8]:
+            raise NotImplementedError(f"got a int-like with a weird width: {width} {repr(tobj)}")
+
+        return f"ctypes.c_{'u' if not signed else ''}int{width*8}"
+    elif tobj_type == PointerType:
+        # check for void target
+        targ = tobj.target
+        if type(targ) == VoidType:
+            return "ctypes.c_void_p"
+
+        subtype = get_ctypes_equiv(tobj.target, prefix)
+        return f"ctypes.POINTER({subtype})"
+    elif tobj_type == FloatType:
+        floatsz = "float"
+        if tobj.width == 4:
+            floatsz = "float"
+        elif tobj.width == 8:
+            floatsz = "double"
+        else:
+            raise NotImplementedError(f"Unknown float with width {tobj.width} {repr(tobj)}")
+        return f"ctypes.c_{floatsz}"
+    elif tobj_type == FunctionType:
+        restype = get_ctypes_equiv(tobj.return_value, prefix)
+        argtypes = ','.join([get_ctypes_equiv(x.type, prefix) for x in tobj.parameters])
+        #TODO calling convention information instead of just CFUNCTYPE
+        return f"CFUNCTYPE(({restype}), {argtypes})"
+    elif tobj_type == VoidType:
+        # probably a function return value
+        # just use void* whatever
+        return "ctypes.c_void_p"
+
+    else:
+        # not in NamedTypeReference, ArrayType, BoolType, CharType, FloatType, FunctionType, IntegerType, PointerType, WideCharType
+        raise NotImplementedError(f"Unimplemented type type in get_ctypes_equiv: {repr(tobj)}")
+
+def full_deref(bv, tname, tobj):
+    while type(tobj) == NamedTypeReferenceType:
+        tobj = gt(bv, tname)
+        if type(tobj) == NamedTypeReferenceType:
+            tname = tobj.name
+    return tobj, tname
 
 def declaration(bv, typename, typeobj, prefix):
     print(f"DBG: declare {typename}")
@@ -100,6 +225,9 @@ def declaration(bv, typename, typeobj, prefix):
 
     if kind in [TypeKind.ENUM, TypeKind.ALIAS]:
         raise Exception(f"Unexpected type needing a forward declaration? {kind}")
+    
+    typeobj, _ = full_deref(bv, typename, typeobj)
+
     return structunion_declaration_template.format(prefix=prefix, typename=typename, kind=kind.baseclass())
 
 def part_definition(bv, typename, typeobj, prefix):
@@ -109,9 +237,15 @@ def part_definition(bv, typename, typeobj, prefix):
     if kind in [TypeKind.ENUM, TypeKind.ALIAS]:
         raise Exception(f"Unexpected type needing a partial definition? {kind}")
 
-    preitems = get_structure_preitems(typeobj, typename, prefix)
+    typeobj, _ = full_deref(bv, typename, typeobj)
 
-    items = get_structure_items(typeobj, typename, prefix)
+    preitems = get_structunion_preitems(bv, typeobj, typename, prefix)
+
+    items = ""
+    if kind == TypeKind.STRUCT:
+        items = get_struct_items(typeobj, typename, prefix)
+    elif kind == TypeKind.UNION:
+        items = get_union_items(typeobj, typename, prefix)
 
     return preitems + structunion_definition_template.format(prefix=prefix, typename=typename, items=items)
 
@@ -119,11 +253,17 @@ def full_definition(bv, typename, typeobj, prefix):
     print(f"DBG: {typename}")
     kind = get_type_kind(bv, typeobj, typename)
 
+    typeobj, _ = full_deref(bv, typename, typeobj)
+
     if kind in [TypeKind.STRUCT, TypeKind.UNION]:
 
-        preitems = get_structure_preitems(typeobj, typename, prefix)
+        preitems = get_structunion_preitems(bv, typeobj, typename, prefix)
 
-        items = get_structure_items(typeobj, typename, prefix)
+        items = ""
+        if kind == TypeKind.STRUCT:
+            items = get_struct_items(typeobj, typename, prefix)
+        elif kind == TypeKind.UNION:
+            items = get_union_items(typeobj, typename, prefix)
 
         return preitems + structunion_template.format(prefix=prefix, typename=typename, kind=kind.baseclass(), items=items)
 
@@ -132,7 +272,7 @@ def full_definition(bv, typename, typeobj, prefix):
         return enum_template.format(prefix=prefix, typename=typename, items=items)
 
     if kind == TypeKind.ALIAS:
-        equiv = get_alias_equiv(typeobj)
+        equiv = get_ctypes_equiv(typeobj, prefix)
         return alias_template.format(prefix=prefix, typename=typename, equiv=equiv)
     raise NotImplementedError(f"Unimplemented definition for kind {kind}")
 
@@ -144,7 +284,6 @@ def get_type_dbg(bv, typename):
         print(f"Warning: type '{typename}' is provided by multiple debug parsers: {', '.join([x[0] for x in res])}")
 
     return res[0][1]
-    
 
 def get_type(bv, typename):
     # should we check and warn if there is a _1 variant?
@@ -179,7 +318,11 @@ def get_type_deps(bv, tobj, tname):
         refobj = gt(bv, tname)
         if refobj is None:
             raise NameError(f"Could not find referenced type {tname}")
-        deps = get_type_deps(bv, refobj, tname)
+
+        name = tname
+        if type(refobj) == NamedTypeReferenceType:
+            name = refobj.name
+        deps = get_type_deps(bv, refobj, name)
     elif tobj_type in [BoolType, CharType, EnumerationType, FloatType, IntegerType, VoidType, WideCharType]:
         # base types, no dependencies
         pass
@@ -276,21 +419,15 @@ def export_some(bv):
                 deps[tname].remove(found)
 
         
-        print("filename", out_f.result)
-        print(report)
-        #TODO output file or as report if empty filename
+    print("filename", out_f.result)
+    print(report)
+    #TODO output file or as report if empty filename
 
     return True
 
 #TODO
 # - Option or command or blob pattern matching for exporting all types
-# - Option to export all in a type archive
-
-"""
-class {prefix + typename}({basetype}):
-    _pack_=1
-    _fields_=[...]
-"""
+# - Option to export a whole type archive
 
 PluginCommand.register("Export types to ctypes", "Export one or more types to ctypes", export_some)
 
